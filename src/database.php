7750b9aa509951e4a491f53465e1d1773a9f3cf1
DGmoN <darkhonin@gmail.com>
2018-10-27 04:10:48 +0200

    Cleaned up database file removing redundencies

--- src/database.php
+++ src/database.php
@@ -63,50 +63,6 @@ class Database{
 	}
 }
 
-function select($data, $on_error=null, $parse_data=null){
-	global $pdo;
-	$query = "SELECT {$data['what']} FROM {$data['from']}".(isset($data['where'])?" WHERE ".$data['where']:"");
-	$str = $pdo->prepare($query);
-	try{
-		$str->execute();
-	}catch(PDOException $e){
-		$code = $e->errorCode();
-		if(!is_callable($on_error))
-			$on_error($code);
-		else
-			throw $e;
-		return 0;
-	}
-	$err = intval($str->errorCode());
-	if($err && is_callable($on_error))
-		$on_error($err);
-	if($parse_data)
-		return $parse_data($str);
-	else return $str->fetchAll();
-}
-
-function update($data){
-	global $pdo;
-	$query = "UPDATE {$data['table']} SET {$data['set']} WHERE ".$data['where'];
-	$str = $pdo->prepare($query);
-	$str->execute();
-}
-
-function insert_into_db($data){
-	global $pdo;
-	$query = "INSERT INTO {$data['tabel']} (".implode(", ", array_keys($data['fields'])).") VALUES ('".implode("', '",$data['fields'])."')";
-	$str = $pdo->prepare($query);
-	$str->execute();
-	$affected_rows = $str->errorCode();
-	if($affected_rows == "23000")
-		return "already exists";
-	$sel = [];
-	foreach($data['fields'] as $n=>$f)
-		array_push($sel, "$n='$f'");
-	$sel = implode(" AND ", $sel);
-	return select(["what" => "id", "from"=>$data['tabel'], "where"=>$sel])[0]['id'];
-}
-
 Database::init();
 
 ?>
